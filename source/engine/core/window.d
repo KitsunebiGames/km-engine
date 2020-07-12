5a7dcdf8fd3d29a03ca7a1efcc30a95f04adfa19
Member1221 <cli-luna@protonmail.com>
2020-07-12 15:50:40 +0200

    AMD support

--- source/engine/core/window.d
+++ source/engine/core/window.d
@@ -49,7 +49,7 @@ public:
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE); // To make macOS happy
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
         window = glfwCreateWindow(640, 480, this.title_.ptr, null, null);
         
     }
