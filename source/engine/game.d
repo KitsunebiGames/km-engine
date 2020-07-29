9049cbb7ad15ad6c5cdebcce83b9570d9af04ab0
Member1221 <clipseypone@gmail.com>
2020-07-29 14:17:20 +0200

    Use func pointers for game init, update and cleanup

--- source/engine/game.d
+++ source/engine/game.d
@@ -5,6 +5,7 @@
     Authors: Luna Nielsen
 */
 module engine.game;
+import bindbc.glfw;
 import engine;
 
 private double previousTime_;
@@ -12,9 +13,25 @@ private double currentTime_;
 private double deltaTime_;
 
 /**
+    Function run when the game is to update
+*/
+void function() gameUpdate;
+
+/**
+    Function run when the game is to initialize
+*/
+void function() gameInit;
+
+/**
+    Function run when the game is to clean up
+*/
+void function() gameCleanup;
+
+/**
     Starts the game loop
 */
-void startGameLoop(void delegate() updateFunc) {
+void startGame() {
+    gameInit();
     resetTime();
     while(!GameWindow.isExitRequested) {
 
@@ -29,7 +46,7 @@ void startGameLoop(void delegate() updateFunc) {
         GameWindow.resetViewport();
 
         // Update and render the game
-        updateFunc();
+        gameUpdate();
 
         // Update the mouse's state
         Mouse.update();
@@ -38,6 +55,9 @@ void startGameLoop(void delegate() updateFunc) {
         GameWindow.swapBuffers();
         GameWindow.update();
     }
+
+    // Game cleanup
+    gameCleanup();
 }
 
 /**
