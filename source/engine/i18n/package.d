baee2cd5441a8c711ad4ea4dde57c5248c261a58
Member1221 <clipseypone@gmail.com>
2020-10-07 02:36:19 +0200

    i18n stub

---
+++ source/engine/i18n/package.d
@@ -0,0 +1,9 @@
+/*
+    Internationalization Subsystem
+
+    Copyright © 2020, Luna Nielsen
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors: Luna Nielsen
+*/
+module engine.i18n;
